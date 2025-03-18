581b09ca1d326a26a7c9dc1b496f0ac5338c22f8
aaa <mvp7109@gmail.com>
2025-03-18 11:50:33 +0900

    1

--- dataset.js
+++ dataset.js
@@ -18,20 +18,27 @@ class DataSet{
 
 let d1 = new DataSet("선형회귀모델")//메인 타이틀 //메뉴번호
 d1.set_content("보스턴 주택 가격 예측 선형 모델")//서브 타이틀
-d1.set_img(0,{imgtitle:"요구사항명세서작성",imgurl:"https://drive.google.com/file/d/1nnSKw0su1NKkr-4O3bXNv33_ZZ8D54Qk/view?usp=drive_link",imglog:"작성된 요구사항 명",sourceurl:"https://docs.google.com/document/d/1fYO-WllL1nq8-qJC6Aj8fTeQplv7hvQzxUfRQAgAamo/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(0,{imgtitle:"보스턴 데이터 수신",imgurl:"https://drive.google.com/file/d/1TgZwp61x45vVZhOPSeN9-IIDLfPRmCbx/view?usp=drive_link",imglog:"텐서플로우 보스턴 데이터셋 수신 코드",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"데이터 특성 파악",imglog:"각 필드별 데이터의 특성의 값을 확인",imgurl:"https://drive.google.com/file/d/1rDEH6nJee7Hi4dEqyKcX8ZWACJZvYIMP/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"데이터 연관성 확인",imglog:"가격정답과 데이터의 특성별 상호 연관도를 파악",imgurl:"https://drive.google.com/file/d/13ltn5tQw0cPusa9DrLR9VtX9bIIERg4P/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"데이터 분포도 파악",imglog:"히스토그램을 이용하여 데이터의 분포와 이상치 데이터 확인",imgurl:"https://drive.google.com/file/d/1nrjXsYPJ97cWZmi7OPwR2FZZDimSXBAp/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"데이터 정규분포 전환",imglog:"훈련 전처리를 위한 연관성이 있는 데이터를 평균 0 표준편차 1로 구성된 정규분포로 변환 ",imgurl:"https://drive.google.com/file/d/1Eg-BMsE2MD8EPVyG1U6E6_R3WvYTkMA9/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"순차모델구성 및 훈련실행",imglog:"평균제곱오차법을 이용한 손실함수와 경사하강법을 이용한 최적화 함수로 컴파일 및 최적화된 훈련 15회 실행",imgurl:"https://drive.google.com/file/d/1e9l-PzORpHwkW-uW-B5bBCtXTJe4was4/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"훈련결과 시각화",imglog:"훈련 결과인 mse 손실값의 시각화 표현",imgurl:"https://drive.google.com/file/d/1t6Nm-ZYC0WZf6Rnz4xZCAffDl12Rhs1B/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+d1.set_img(0,{imgtitle:"모델의 예측결과 측정",imglog:"테스트 데이터를 주입하여 예측결과를 인출하고 실제 정답과 차이를 ",imgurl:"https://drive.google.com/file/d/1YdeZKYxbEB4IDbzX3_F7u4_mJibbMYvR/view?usp=drive_link",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
 
-d1.set_content("목표 시스템 개발을 위한 적정한 언어를 선정하고 라이선스와 향후 지원사항을 참고하여 결정한다.")
-d1.set_img(1,{imgtitle:"개발언어선정",imgurl:"https://drive.google.com/file/d/15GgAeFgW1Cxctk9PbJagUNfG2UkEU4WS/view?usp=drive_link",imglog:"자바언어기반 스프링프레임워크, html5, memberData file",sourceurl:"https://docs.google.com/document/d/1UvD1fZNL9yjLORwxgAW5vD6C-A_RwL1zcNan9RZqmys/edit?usp=drive_link"})//이미지타이틀
-d1.set_img(1,{imgtitle:"스프링프레임워크",imgurl:"https://drive.google.com/file/d/1HRUQhj2Tk6AzIW_NSRD59wGUAduJYTmW/view?usp=drive_link",imglog:"오픈소스 스프링프레임워크 v4.221",sourceurl:""})//이미지타이틀
 
 
-d1.set_content("코딩개발도구와 테스트 및 빌드, 패키징, 배포등 개발 전반에 대한 필요한 환경을 설정한다.")
-d1.set_img(2,{imgtitle:"개발환경 구축 명세",imgurl:"https://drive.google.com/file/d/1mw6ZBtTbbZQYlfz1YQXCuTfoi-hnvfpg/view?usp=drive_link",imglog:"운영체제 및 개발에 필요한 환경을 고려하여 개발도구, 테스트도구, 빌드도구, 패키징도구, 형상관리도구등을 결정",sourceurl:"https://docs.google.com/document/d/1rNVbHnhYGoozaqX_RsD3cErmKYzGUMvba9ihxbP2CKk/edit?usp=drive_link"})//이미지타이틀
-d1.set_img(2,{imgtitle:"개발도구 Spring for Eclipse",imgurl:"https://drive.google.com/file/d/1vl3Ridea05eDn3Hx0cfIWzgkPi-vQX31/view?usp=drive_link",imglog:"자바 스프링 서버단 개발을 위한 도구",sourceurl:""})//이미지타이틀
-d1.set_img(2,{imgtitle:"빌드도구 gradle",imgurl:"https://drive.google.com/file/d/1vlxyuzBmzaBk7B9VoycUH0mRBBxe4nTr/view?usp=drive_link",imglog:"프로그램 빌드 및 외부 라이브러리 디펜더시를 위한 도구",sourceurl:""})//이미지타이틀
-d1.set_img(2,{imgtitle:"형상관리도구 git",imgurl:"https://drive.google.com/file/d/1yLXiqHWblzxAnZXGZKvRcxU8HSgf3UqS/view?usp=drive_link",imglog:"프로그램의 전반적인 개발협업 및 버전관를 위한 도구",sourceurl:""})//이미지타이틀
 
-d1.set_fill("서버의 보안성과 각 수행기능별 패턴을 분리하기 위해 Spring WAS 와 사용자 View 기능을 위해 웹브라우져에서 일반적인 작동이 가능한 HTML5 를 구현하며 데이터베이스 연동 대신 파일로 회원목록 저장")//사용자 에필로그
+d1.set_content("캘리포니아 주택 가격 예측 선형 회귀모델")
+d1.set_img(1,{imgtitle:"캘리포니아 주택 가격 예측 선형 회귀모델",imgurl:"",imglog:"",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+
+
+
+d1.set_content("당뇨상태 1년후 예측 선형 회귀모델")
+d1.set_img(2,{imgtitle:"1년후 당뇨상태 예측",imgurl:"",imglog:"",sourceurl:"https://github.com/qwoiuerjfsd/ai_0317/blob/main/LinearRegression/examp_LinearRegression_bostonHousing.py"})//이미지타이틀
+
+d1.set_fill("선형 회귀 모델은 단일데이터를 이용하거나 다중 데이터를 이용하여 연속적인 값을 출력하여 예측한다")//사용자 에필로그
 data_sets.push(d1)
 
 // menu2 =============================================================
